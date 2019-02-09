1abf9e565c54de7bcc413cb106d441e7299e7d73
RuisongZhou <811437508@qq,com>
2019-02-09 23:33:20 +0800

    finish the model

--- textCNN/model/textCNN_model.py
+++ textCNN/model/textCNN_model.py
@@ -52,35 +52,143 @@ class TextCNN():
 
 
             #3 conv layer + maxpool layer for each filer size
-            with tf.name_scope('dropout_layer'):
                 pool_layer_list = []
                 for filter_size in filter_sizes:
                     max_pool_layer = self.add_conv_layer(filter_size, filter_numbers)
                     pool_layer_list.append(max_pool_layer)
             
+
             # 4. Fully-connection layer
             # use softmax and L2 normolization
             # combine all the max pool --feature
+            with tf.name_scope('dropout_layer'):
+                
+                max_num = len(filter_sizes)*self.filter_numbers
+                h_pool = tf.contat(pool_layer_list,name='last_pool_layer', axis = 3)
+                pool_layer_flat = tf.reshape(h_pool,[-1,max_num],name='pool_layer_flat')
+
+                dropout_pro_layer = tf.nn.dropout(pool_layer_flat,self.dropout_pro,name='droupout')
+            
+            with tf.name_scope('soft_max_layer'):
+                softmax_w = tf.Variable(tf.truncated_normal([max_num,2],stddev=0.01),name='softmax_liner_weight')
+                self.variables_summery(softmax_w)
+
+                softmax_b = tf.Variable(tf.constant(0.1,shape=[2]), name='softmax_linear_bias')
+                self.variables_summery(softmax_b)
+
+                self.l2_loss += tf.nn.l2_loss(softmax_w)
+                self.l2_loss += tf.nn.l2_loss(softmax_b)
+
+                self.softmax_values = tf.nn.xw_plus_b(dropout_pro_layer,softmax_w,softmax_b,name='soft_values')
+                self.predictions = tf.argmax(self.softmax_values, axis=1, name='predictions', output_type=tf.int32)
+
+
+            with tf.name_scope('loss'):
+                losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.softmax_values,labels=self.input_y)
+                self.loss = tf.reduce_mean(losses) + 0.001*self.l2_loss
+
+                tf.summary.scalar('last_loss', self.accuracy  )
+
+            with tf.name_scope('train'):
+                optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+                self.train_op = optimizer.minimize(self.loss)
 
 
 
+            self.session = tf.InteractiveSession(graph=self.train_graph)
+            self.merged = tf.summary.merge_all()
+            self.train_writter = tf.summary.FileWriter('../result/', graph=self.train_graph)
 
-        
-        def train(self,train_x,train_y):
-            pass
-        
 
-        def validatation(self,text_x, text_y):
-            pass
 
-        def close(self):
-            pass
+    
+    def train(self,train_x,train_y):
+        self.session.run(tf.global_variables_initializer())
+        #迭代训练
+        for epoch in range(self.epochs):
+            # pdb.set_trace()
+            train_batch = self.__get_batchs(train_x, train_y, self.batch_size)
+            train_loss, train_acc, count = 0.0, 0.0, 0
+            for batch_i in range(len(train_x)//self.batch_size):
+                x,y = next(train_batch)
+                feed = {
+                    self.input_x:x,
+                    self.input_y:y,
+                    self.dropout_pro:self.dropout_pro_item,
+                    self.learning_rate:self.learning_rate_item
+                }
+                _,summarys,loss,accuracy = self.session.run([self.train_op,self.merged,self.loss,self.accuracy],feed_dict=feed)
+                train_loss, train_acc, count = train_loss + loss, train_acc + accuracy, count + 1
+                self.train_writer.add_summary(summarys,epoch)
+                # each 5 batch print log
+                if (batch_i+1) % 15 == 0:
+                    print('Epoch {:>3} Batch {:>4}/{} train_loss = {:.3f} accuracy = {:.3f}'.
+                          format(epoch,batch_i,(len(train_x)//self.batch_size),train_loss/float(count),train_acc/float(count)))
 
-        def get_batches(self):
-            pass
-        
-        def add_conv_layer(self):
-            pass
+    
+    def validatation(self,text_x, text_y):
+        test_batch = self.get_batches(text_x, text_y, self.batch_size)
+        eval_loss, eval_acc, count = 0.0, 0.0, 0.0
+        for batch in range(len(text_x)// self.batch_size):
+            x,y = next(test_batch)
+            feed = {
+                self.embedding_layer: x,
+                self.input_y :y,
+                self.dropout_pro: self.dropout_pro_item,
+                self.learning_rate:1.0
+            }
+            loss, accuracy = self.session.run([self.loss, self.accuracy],feed_dict=feed)
+            eval_loss, eval_acc, count = eval_loss+loss, eval_acc+accuracy, count+1
 
-        def variables_summery(self):
-            pass
+        return eval_acc/float(count), eval_loss/float(count)
+            
+
+    def close(self):
+        self.session.close()
+        self.train_writer.close()
+
+    #generate batches
+    def get_batches(self,Xs, Ys, batch_size):
+        for start in range(0, len(Xs), batch_size):
+            end = min(start+batch_size,len(Xs))
+            yield Xs[start:end], Ys[start:end]
+    
+    def add_conv_layer(self,filter_size,filter_numbers):
+        with tf.name_scope('conv_maxpool-size%d'%(filter_size)):
+            #卷积层
+            filter_shape = [filter_size, self.embedding_size, 1, filter_numbers]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='filter_weight')
+            self.variables_summery(W)
+            b = tf.Variable(tf.constant(0.1, shape=[filter_numbers], name='filter_bias'))
+            self.variables_summery(b)
+
+
+            #参数说明
+            #第一个参数input：指需要做卷积的输入图像 [训练时一个batch的图片数量, 图片高度, 图片宽度, 图像通道数]
+            #第二个参数filter：相当于CNN中的卷积核 [卷积核的高度，卷积核的宽度，图像通道数，卷积核个数]
+            #第三个参数strides：卷积时在图像每一维的步长，这是一个一维的向量，长度4,
+            #第四个参数padding：string类型的量，只能是"SAME","VALID"其中之一，这个值决定了不同的卷积方式
+            #第五个参数：use_cudnn_on_gpu: bool类型，是否使用cudnn加速，默认为true
+            conv_layer = tf.nn.conv2d(self.embedding_layer_expand,W,strides=[1,1,1,1],padding='VALID',name='conv_layer')
+            relu_layer = tf.nn.relu(tf.nn.bias_add(conv_layer,b),name='relu_layer')
+
+            max_pool_layer = tf.nn.max_pool(relu_layer,ksize=[1, self.sentence_length-filter_size+1, 1, 1],strides=[1,1,1,1],padding='VALID',name='maxpool')
+            return max_pool_layer
+
+
+    def variables_summery(self,var):
+        """
+        :param var: Tensor, Attach a lot of summaries to a Tensor (for TensorBoard visualization).
+        """
+        with tf.name_scope('summeries'):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)  # 记录参数的均值
+
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+                tf.summary.scalar('stddev', stddev)
+                tf.summary.scalar('max', tf.reduce_max(var))
+                tf.summary.scalar('min', tf.reduce_min(var))
+
+                # 用直方图记录参数的分布
+                tf.summary.histogram('histogram', var)
